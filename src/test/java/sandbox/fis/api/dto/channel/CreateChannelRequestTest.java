6b809155b36d81763089773831eac9cdc9b6e6c0
SpearZero <41422767+SpearZero@users.noreply.github.com>
2022-02-28 21:03:07 +0900

    test: Channel DTO 테스트 추가

--- src/test/java/sandbox/fis/api/dto/channel/CreateChannelRequestTest.java
+++ src/test/java/sandbox/fis/api/dto/channel/CreateChannelRequestTest.java
@@ -41,4 +41,40 @@ class CreateChannelRequestTest {
         // then
         assertThat(validate.isEmpty()).isFalse();
     }
+
+    static Stream<String> overLengthValue() {
+        return Stream.of("채널명30자이상채널명30자이상채널명30자이상채널명30자이");
+    }
+
+    @ParameterizedTest(name = "{index} - input channelName = {0}")
+    @MethodSource("overLengthValue")
+    @DisplayName("채널명이 30자 이상 입력될 경우 검증이 통과되지 않음")
+    void CreateChannelRequest_channelName_length_over_then_CreateChannelRequest_channelName_fail(String channelName) {
+        // given
+        CreateChannelRequest createChannelRequest = new CreateChannelRequest(channelName);
+
+        // when
+        Set<ConstraintViolation<CreateChannelRequest>> validate = validator.validate(createChannelRequest);
+
+        // then
+        assertThat(validate.isEmpty()).isFalse();
+    }
+
+    static Stream<String> validValue() {
+        return Stream.of("채널명", "채널입니다", "채널채널");
+    }
+
+    @ParameterizedTest(name = "{index} - input channelName = {0}")
+    @MethodSource("validValue")
+    @DisplayName("채널명에 유효한 값이 입력될 경우 검증이 통과됨")
+    void CreateChannelRequest_channelName_valid_then_CreateChannelRequest_channelName_success(String channelName) {
+        // given
+        CreateChannelRequest createChannelRequest = new CreateChannelRequest(channelName);
+
+        // when
+        Set<ConstraintViolation<CreateChannelRequest>> validate = validator.validate(createChannelRequest);
+
+        // then
+        assertThat(validate.isEmpty()).isTrue();
+    }
 }
