c8c556dc97f65ea805e4892a75a3018dc5421d51
SpearZero <41422767+SpearZero@users.noreply.github.com>
2022-02-28 19:16:46 +0900

    first commit

---
+++ src/test/java/sandbox/fis/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package sandbox.fis.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
